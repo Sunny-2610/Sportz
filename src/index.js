279797f4cab7826d42f4d790baf8455b6a6c3dd9
Sunny-2610 <sunnysinha9884@gmail.com>
2026-04-29 12:54:13 +0530

    finding bug in commentry

--- src/index.js
+++ src/index.js
@@ -3,10 +3,10 @@ AgentAPI.config();
 
 import express from 'express';
 import http from 'http';
-import {matchRouter} from "./db/routes/matches.js";
+import {matchRouter} from "./routes/matches.js";
 import {attachWebSocketServer} from "./ws/server.js";
- import {securityMiddleware} from "./arcjet.js";
- //import {commentaryRouter} from "./routes/commentary.js";
+import {securityMiddleware} from "./arcjet.js";
+import {commentaryRouter} from "./routes/commentary.js";
 
 const PORT = Number(process.env.PORT || 8000);
 const HOST = process.env.HOST || '0.0.0.0';
@@ -20,10 +20,10 @@ app.get('/', (req, res) => {
   res.send('Hello from Express server!');
 });
 
- app.use(securityMiddleware());
+// app.use(securityMiddleware());
 
 app.use('/matches', matchRouter);
-// app.use('/matches/:id/commentary', commentaryRouter);
+app.use('/matches/:id/commentary', commentaryRouter);
 
 const { broadcastMatchCreated, broadcastCommentary } = attachWebSocketServer(server);
 app.locals.broadcastMatchCreated = broadcastMatchCreated;
